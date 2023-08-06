95e6e87845c1d7c25782a78a06287af07f674d15
SidStraw <j80304xz@gmail.com>
2023-08-06 21:54:43 +0800

    feat: ProjectList

---
+++ pages/components/ProjectList.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+
+const list = reactive<{ html?: string; src?: string }[]>([
+  {
+    html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UNHV3xLILEg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+  },
+  {
+    html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/x_pf7CotP0I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+  },
+  {
+    html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/cL4iVjNA3dY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+  },
+])
+</script>
+
+<template>
+  <div class="py-6">
+    <SHeading class="text-zinc-800">呱呱企劃</SHeading>
+    <Carousel :items-to-show="1">
+      <Slide v-for="item in list" :key="item.html || item.src">
+        <SIframe class="w-full" v-bind="item" />
+      </Slide>
+
+      <template v-if="list.length" #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+:deep(button.carousel__prev),
+:deep(button.carousel__next) {
+  @apply rounded-full bg-gray-100;
+}
+:deep(button.carousel__prev--disabled),
+:deep(button.carousel__next--disabled) {
+  @apply hidden;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import HeroPhoto from './components/HeroPhoto.vue'
+import ProjectList from './components/ProjectList.vue'
 </script>
 
 <template>
   <HeroPhoto />
+  <div class="relative bg-[url('/5w.png')] bg-cover bg-fixed bg-center">
+    <div class="absolute inset-0 bg-blend-multiply backdrop-blur" />
+    <div class="mx-auto max-w-5xl backdrop-blur">
+      <ProjectList />
+    </div>
+  </div>
 </template>
