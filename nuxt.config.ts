3735ef189a6311319e91d42a205668de0ebe112c
SidStraw <j80304xz@gmail.com>
2023-09-30 02:12:26 +0800

    feat: add lazy load

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,26 @@
 export default defineNuxtConfig({
-  modules: ['@vueuse/nuxt', '@nuxtjs/color-mode'],
+  modules: ['@vueuse/nuxt', '@nuxtjs/color-mode', 'nuxt-lazy-load'],
   extends: 'content-wind',
   colorMode: {
     preference: 'light', // default value of $colorMode.preference
     fallback: 'light', // fallback value if not system preference found
     classSuffix: '',
+  },
+
+  lazyLoad: {
+    images: true,
+    videos: true,
+    audios: true,
+    iframes: false,
+    native: false,
+    directiveOnly: false,
+
+    // 預設圖片必須位於公用資料夾中
+    defaultImage: '/img/logo.png',
+
+    // 刪除類別設定值為 false
+    loadingClass: 'isLoading',
+    returnedClass: 'isLoaded',
+    appendClass: 'lazyLoad',
   },
 })
