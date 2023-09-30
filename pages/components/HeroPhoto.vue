935ce0df0fce3358d87cb4c90c61bc0f2f3d6e1c
SidStraw <j80304xz@gmail.com>
2023-09-30 08:32:39 +0800

    feat: modify hero photo

--- pages/components/HeroPhoto.vue
+++ pages/components/HeroPhoto.vue
@@ -1,55 +1,29 @@
 <script setup lang="ts">
-const debutDate = dayjs('2022-09-25 20:00:00+08:00')
-const nowRef = ref(dayjs())
-const daysPassed = computed(() => {
-  const now = nowRef.value
-  const diff = now.diff(debutDate)
-  const duration = dayjs.duration(diff)
-
-  return [
-    {
-      type: 'Days',
-      value: duration.asDays().toFixed(),
-    },
-    {
-      type: 'Hours',
-      value: duration.hours(),
-    },
-    {
-      type: 'Minutes',
-      value: duration.minutes(),
-    },
-    {
-      type: 'Seconds',
-      value: duration.seconds(),
-    },
-  ]
-})
-
-const intervalId = setInterval(() => (nowRef.value = dayjs()), 1000)
-onUnmounted(() => clearInterval(intervalId))
+const store = useLoadingStore()
 </script>
 
 <template>
   <!-- eslint-disable tailwindcss/no-custom-classname -->
   <div
     id="header"
-    class="relative flex h-screen flex-col items-center justify-center bg-black/50 bg-[url('/cover.jpg')] bg-cover bg-[center_25%] bg-blend-multiply"
+    :key="Date.now()"
+    class="relative flex h-screen items-center justify-center bg-[url('/assets/20230813.jpg')] bg-cover bg-fixed lg:bg-teal-50 lg:bg-[url('/assets/bg.png')]"
   >
-    <div class="flex flex-col items-center drop-shadow-lg">
-      <SHeading h1 class="text-white shadow-red-800 text-shadow">嘿！呱們～</SHeading>
-      <h2 class="mt-2 text-4xl font-semibold text-white">吃早餐了嗎？</h2>
-    </div>
-    <div class="mt-8 flex gap-4">
+    <template v-if="store.isLoaded">
       <div
-        v-for="date in daysPassed"
-        :key="date.type"
-        class="animate__animated animate__pulse animate__slow flex h-16 w-16 animate-ping flex-col items-center justify-center rounded-full bg-cyan-800/80 text-white sm:h-24 sm:w-24"
+        class="animate__animated animate__fadeInLeft hidden h-full flex-1 overflow-hidden bg-[url('/assets/20230813.jpg')] bg-cover bg-fixed lg:block"
+      />
+      <article
+        class="animate__animated animate__zoomIn animate__slower self-end rounded-lg bg-teal-50/75 p-8 pb-16 lg:self-center lg:p-12 xl:p-24"
       >
-        <p class="text-2xl md:text-4xl">{{ date.value }}</p>
-        <p class="text-xs md:text-base">{{ date.type }}</p>
-      </div>
-    </div>
+        <SHeading h1>柴崎楓音</SHeading>
+        <SHeading h3 class="mb-4 text-5xl">呱們的快樂柴犬</SHeading>
+        <div class="text-2xl font-bold text-teal-500 shadow-gray-300 text-shadow-border sm:text-3xl">
+          <p>來跟我一起吃吃早餐聊聊天吧✨</p>
+          <p>還有好多想一起做的事情呢…🍁</p>
+        </div>
+      </article>
+    </template>
     <img src="/mask-bottom.png" alt="mask" class="absolute bottom-0" />
   </div>
 </template>
