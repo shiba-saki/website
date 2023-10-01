618287c3d99ecd5e59c93d0eb25ed9aac8b0c449
SidStraw <j80304xz@gmail.com>
2023-10-01 23:28:57 +0800

    fix: UI

--- components/MessageCard.vue
+++ components/MessageCard.vue
@@ -14,10 +14,10 @@ withDefaults(
 
 <template>
   <div
-    class="flex w-[472px] flex-col items-center justify-center bg-[url('/assets/message.png')] bg-contain font-bold text-teal-700"
-    :class="small ? 'h-[293px]' : 'h-full pb-[25px] pl-[75px] pr-[100px] pt-[75px]'"
+    class="flex w-[472px] flex-col items-center justify-center bg-[url('/assets/message.png')] bg-contain bg-no-repeat font-bold text-teal-700"
+    :class="small ? 'h-[293px]' : 'h-full pb-[25px] pl-[75px] pr-[80px] pt-[75px]'"
   >
     <p class="text-2xl shadow-white text-shadow">{{ name }}</p>
-    <p v-if="!small" class="mt-4" v-html="message.replace('\n', '<br />')"></p>
+    <p v-if="!small" class="mt-2 flex-1 text-sm md:text-base" v-html="message.replace('\n', '<br />')"></p>
   </div>
 </template>
